89d9b8cf5deea1b2c9b0f3872d3b9e2a9e4f46f8
PANG2OPPA <khbkhb85098509@gmail.com>
2024-07-04 18:32:48 +0900

    Spring Boot를 이용한 익명 게시판 / 기본 세팅 진행중...

---
+++ src/main/java/com/pang2oppa/AnonBoard/AnonBoardApplication.java
@@ -0,0 +1,13 @@
+package com.pang2oppa.AnonBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pang2oppa/AnonBoard/AnonBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pang2oppa.AnonBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
